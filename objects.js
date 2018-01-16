1787a7a3d6a2141d0f3932c27231cc9b26e8478c
Stephanie <s.a.james5@gmail.com>
2018-01-16 21:54:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = {michaelJackson: "thriller"}
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Phil Ochs, "Here'stothestate"
+  return Phil Ochs, "Here's to the state of Mississippi"
 }
