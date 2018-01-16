0497946248a4d8a550386893f16f52940a5b66fb
Stephanie <s.a.james5@gmail.com>
2018-01-16 22:48:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ function updatePlaylist(playlist, artistName, songTitle) {playlist[artistName] =
 return playlist
 }
 function removeFromPlaylist(playlist, artistName)
-{delete playlist["artistName"]
+{delete ["artistName"]
 return playlist
 }
